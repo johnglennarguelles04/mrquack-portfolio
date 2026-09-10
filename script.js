ccde8150ec2f3265e15d2c89a849628191c1743c
Quack <jg.arguelles02@gmail.com>
2026-09-10 15:51:32 +0800

    Refactor form reset logic in script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const form = document.querySelector("#contact form");
 
-form.addEventListener("submit", function() {
-    form.reset();
-});
-
 window.addEventListener("pageshow", function() {
     form.reset();
 });
